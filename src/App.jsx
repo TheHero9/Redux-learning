80b000fa3592cb1c7c6720f07dac6f4378a396a0
Demetrios Vlassis <95277454+TheHero9@users.noreply.github.com>
2023-02-15 18:23:41 +0200

    learning session 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,11 @@ import './App.css'
 import {useSelector, useDispatch} from 'react-redux'
 
 function App() {
- const counter = useSelector( state => state.counter);
- const dispatch = useDispatch();
-
- const increment = (nr) => {
-    dispatch({type: 'INC'});
- }
-
- const decrement = () =>{
-  dispatch({type: "DEC"})
-
- }
 
   return (
     <div className="App">
       
       <h1>Counter App</h1>
-      <h2>{counter}</h2>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,38 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
-import store from './Store'
+
+
+import store from './store' 
+
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed!", store.getState())
+})
+
+store.dispatch({
+  type: 'bugAdded',
+  payload: {
+    description: "Bug1",
+  }
+})
+
+unsubscribe()
+
+store.dispatch({
+  type: 'bugRemoved',
+  payload: {
+    id: 1
+  }
+})
+
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+
 )
