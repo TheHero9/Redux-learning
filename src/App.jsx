5dbd28a32600f2d9e61246b6ce4e1c7ce699945b
Demetrios Vlassis <95277454+TheHero9@users.noreply.github.com>
2023-02-16 16:02:39 +0200

    lesson 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,37 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import {useSelector, useDispatch} from 'react-redux'
 
+import store from './main'
+
 function App() {
+
+
+  const counter = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const increment = () =>{
+    return dispatch({type: 'increment'})
+  }
+
+  const decrement = () => {
+    return dispatch({type: 'decrement'})
+  }
+
+  const square =() => {
+    return dispatch ({type: 'square'})
+  }
+
+
+
 
   return (
     <div className="App">
       
       <h1>Counter App</h1>
+      <h4>{counter}</h4>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={square}>^</button>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,31 +4,29 @@ import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
 
+import {createStore} from 'redux'
 
-import store from './store' 
+const reducer = (state = 0, action) => {
+
+  switch(action.type){
+    case 'increment':
+      return state += 1
+      break;
+    case 'decrement':
+      return state -= 1
+      break;
+    case 'square':
+      return state = state**2
+      break;
+    default: 
+      return state
 
 
-const unsubscribe = store.subscribe(() => {
-  console.log("Store changed!", store.getState())
-})
-
-store.dispatch({
-  type: 'bugAdded',
-  payload: {
-    description: "Bug1",
   }
-})
 
-unsubscribe()
+}
 
-store.dispatch({
-  type: 'bugRemoved',
-  payload: {
-    id: 1
-  }
-})
-
-console.log(store.getState())
+const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
@@ -38,3 +36,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
 
 )
+
+
+export default store
